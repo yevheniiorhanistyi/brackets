1e60e9de14c4f900e1e072c0dde28f66cc6c6fb6
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-09-02 18:59:08 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsMap = {};
+
+  for (let [open, close] of bracketsConfig) {
+    bracketsMap[close] = open;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentChar = str[i];
+
+    if (Object.values(bracketsMap).includes(currentChar)) {
+      if (bracketsMap[currentChar] === currentChar && stack[stack.length - 1] === currentChar) {
+        stack.pop();
+      } else {
+        stack.push(currentChar);
+      }
+    } else if (bracketsMap.hasOwnProperty(currentChar)) {
+      if (stack.length === 0 || stack.pop() !== bracketsMap[currentChar]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
